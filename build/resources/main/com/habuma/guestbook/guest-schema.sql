25c9067d6c4f2438bf70fb38ff50a62281d4c449
Craig Walls <cwalls@vmware.com>
2012-08-25 07:19:25 -0500

    Removed bin/build directories

--- build/resources/main/com/habuma/guestbook/guest-schema.sql
+++
@@ -1,8 +0,0 @@
-create table Guest (
-	id identity,
-	name varchar not null,
-	email varchar not null,
-	dateOfVisit timestamp not null
-);
-
-insert into Guest (name, email, dateOfVisit) values ('Craig', 'craig@habuma.com', current_timestamp());
